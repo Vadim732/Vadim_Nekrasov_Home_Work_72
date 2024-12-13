8972fb1aceeb3b595c52a6d102f9d341c1381629
Ruzakhunova Maria <original989original@gmail.com>
2024-12-13 16:44:25 +0600

    Added  index page with links to all establishments and the detailed view page with information about the establishment and its dishes.

--- Delivery/Delivery/Controllers/EstablishmentController.cs
+++ Delivery/Delivery/Controllers/EstablishmentController.cs
@@ -1,11 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Delivery.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Delivery.Controllers;
 
 public class EstablishmentController : Controller
 {
-    public IActionResult Index()
+    private readonly DeliveryContext _context;
+
+    public EstablishmentController(DeliveryContext context)
     {
-        return View();
+        _context = context;
     }
+    
+    public async Task<IActionResult> Index()
+    {
+        List<Establishment> establishments = await _context.Establishments.ToListAsync();
+        return View(establishments);
+    }
+
+    public async Task<IActionResult> EstablishmentDetailsPage(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+        
+        Establishment? establishment = await _context.Establishments.Include(e => e.Dishes).FirstOrDefaultAsync(e => e.Id == id);
+        
+        if (establishment == null)
+        {
+            return NotFound();
+        }
+        return View(establishment);
+    }
+    
 }
